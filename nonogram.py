c00c94e795dc5b19b84e72d3e1d60ab075757ec8
An Hoang Nguyen <nguyenan.sadek@outlook.com>
2025-02-06 11:42:11 +0700

    Xong phan cot loi cua game

--- nonogram.py
+++ nonogram.py
@@ -1,4 +1,32 @@
 import numpy as np
 
-def create_row(random_state):
-    pass
+np.random.seed(42)
+mtx = np.random.randint(0, 2, size=(10, 10))
+
+def extract_clues(size, matrix):
+    row_clues = []
+
+    for row in matrix:
+        clue = []
+        count = 0  
+        
+        for col in range(size):
+            if row[col] == 1:
+                count += 1 
+            else:
+                if count > 0:  
+                    clue.append(count)
+                    count = 0  
+        
+        if count > 0: 
+            clue.append(count)
+
+        row_clues.append(clue)
+
+    return row_clues
+
+print(mtx)
+r_clues = extract_clues(10, mtx)
+c_clues = extract_clues(10, mtx.T)
+print(r_clues)
+print(c_clues)
